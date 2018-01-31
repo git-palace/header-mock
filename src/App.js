cef94d096777c4c506556e3b8ba4cb26933569c3
git-palace <edeveloper4@gmail.com>
2018-01-31 10:14:29 -0800

    complete header top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import lang_us from './assets/icons/lang-us.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="header"></div>
+      <div className="header">
+        <div className="top">
+          <div className="search-container ml-auto">
+            <div className="search">
+              <input className="min-width-250px" type="text" placeholder="Search by City or Zip" />
+              <i className="icon icon-search"></i>
+            </div>
+          </div>
+
+          <div className="contact-info">
+            <div className="lang-list">
+              <div className="selected">
+                <span>US</span>&nbsp;<img src={lang_us} alt="lang-us" />
+              </div>
+            </div>
+
+            <div className="phone">
+              <h1 className="phone-number">
+                <i className="icon icon-telephone"></i>
+                <span>123-456-7890</span>
+              </h1>
+
+              <p className="desc">Talk to a local advisor for FREE</p>
+            </div>
+          </div>
+        </div>
+      </div >
     );
   }
 }
